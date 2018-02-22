178a6e58b7ac9f699bdb83d50186831d0dc08952
PepijnKok <AtlasTheTitan77@gmail.com>
2018-02-22 12:35:23 +0100

    idk what this is but i hope it wont fuck up

---
+++ Projects/Mobile/Assets/TestFiles/UIdelete.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIdelete : MonoBehaviour {
+	public Image nav;
+	public Image Bone;
+	public Image Btwo;
+	public Image Return;
+	public float Spedo;
+	public void navStart(){
+	if(!nav.gameObject.active){
+		nav.gameObject.SetActive(true);
+		StartCoroutine(StartSlide(Spedo));
+	} else if(nav.gameObject.active){
+		nav.gameObject.SetActive(true);
+		StartCoroutine(StartSlide(-Spedo));
+	}
+	}
+
+	IEnumerator StartSlide(float i){
+		yield return new WaitForSeconds(0.01f);
+		nav.fillAmount = nav.fillAmount + (i / 2);
+		Bone.fillAmount = Bone.fillAmount + i;
+		Btwo.fillAmount = Btwo.fillAmount + i;
+		Return.fillAmount = Return.fillAmount + i;
+		if(nav.fillAmount<0.55f && nav.fillAmount>0){
+			StartCoroutine(StartSlide(i));
+		} else if(nav.fillAmount==0){
+			nav.gameObject.SetActive(false);
+		}
+	}
+}
