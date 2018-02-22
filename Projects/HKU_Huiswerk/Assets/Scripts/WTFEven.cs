0e23c1cebb2384beb6172f7308fc6431d7fc14d8
PepijnKok <AtlasTheTitan77@gmail.com>
2018-02-22 14:26:59 +0100

    Idk man

---
+++ Projects/HKU_Huiswerk/Assets/Scripts/WTFEven.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WTFEven : MonoBehaviour {
+
+	void OnMouseDown(){
+		GetComponent<Renderer> ().material.color = Color.red;
+	}
+}
