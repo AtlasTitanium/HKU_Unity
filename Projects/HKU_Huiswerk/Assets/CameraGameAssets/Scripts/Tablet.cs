078e52369e987448b734350d57714909271ac138
PepijnKok <AtlasTheTitan77@gmail.com>
2018-02-23 21:30:45 +0100

    Goodshit

---
+++ Projects/HKU_Huiswerk/Assets/CameraGameAssets/Scripts/Tablet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tablet : MonoBehaviour {
+    public GameObject tablet;
+	bool flag = true;
+ 
+ 	void Update () {
+     	if(Input.GetKeyDown(KeyCode.Tab)){
+			flag = !flag; //set flag to opposite value
+			Debug.Log("flag is " + flag);
+			changer();
+     	}
+ 	}
+	
+	void changer(){
+		if(flag){       
+         	tablet.SetActive(true);
+			Debug.Log("tablet is " + tablet.activeSelf);
+			tablet.GetComponent<Animation>().Play();
+     	} else {
+         	tablet.SetActive(false);
+			Debug.Log("tablet is " + tablet.activeSelf);
+			tablet.GetComponent<Animation>().Play();
+     	}
+	}
+}
